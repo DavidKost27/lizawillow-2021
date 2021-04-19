aee6d40281e4188559fb6d5935e11020a303c175
David Kostuchenko <davidkost1999@gmail.com>
2021-04-19 10:24:18 +0300

    [Update] Menu animations
    - Added basic "Menu" animation when opening the menu.
    * More adjustments will be needed , this is a basic animation and not final.

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,19 +3,72 @@ import "./styles.scss";
 import { motion } from "framer-motion";
 
 export default function Menu(props) {
-  const variants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: "-100%" },
+  const menuVariants = {
+    open: {
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        restDelta: 2,
+      },
+    },
+    closed: {
+      y: "-100vh",
+      transition: {
+        delay: 1,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+  const menuItemVariants = {
+    open: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: {
+          stiffness: 1000,
+          velocity: -100,
+        },
+        staggerChildren: 0.07,
+        delayChildren: 0.2,
+      },
+    },
+    closed: {
+      y: "-100vh",
+      opacity: 0,
+      transition: {
+        y: { stiffness: 1000 },
+        staggerChildren: 0.05,
+        staggerDirection: -1,
+      },
+    },
   };
   return (
     <motion.div
       animate={props.isOpen ? "open" : "closed"}
-      variants={variants}
+      variants={menuVariants}
       className="menu-container"
     >
-      <div className="menu-container__home center">Home</div>
-      <div className="menu-container__about center">About</div>
-      <div className="menu-container__contact center">Contact</div>
+      <motion.div
+        variants={menuItemVariants}
+        className="menu-container__home center"
+      >
+        Home
+      </motion.div>
+      <motion.div
+        variants={menuItemVariants}
+        className="menu-container__about center"
+      >
+        About
+      </motion.div>
+      <motion.div
+        variants={menuItemVariants}
+        className="menu-container__contact center"
+      >
+        Contact
+      </motion.div>
     </motion.div>
   );
 }
