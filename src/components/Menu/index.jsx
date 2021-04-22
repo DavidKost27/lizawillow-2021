80fb800572101ab3e2b46ba92ff5dfd2dbe8b342
David Kostuchenko <davidkost1999@gmail.com>
2021-04-22 14:18:23 +0300

    [Update] Added Link ActiveStyles
    - Added indication of what page you are at.

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,25 +4,40 @@ import { motion } from "framer-motion";
 import { Link } from "gatsby";
 
 export default function Menu(props) {
+  const screenSize = window.screen.width;
   const { toggleMenu } = props;
-  const menuVariants = {
-    open: {
-      y: 0,
-    },
-    closed: {
-      y: "-100vh",
-    },
+  let menuVariants;
+  let menuItemVariants;
+  if (screenSize >= 1230) {
+    menuVariants = {};
+    menuItemVariants = {};
+  } else {
+    menuVariants = {
+      open: {
+        y: 0,
+      },
+      closed: {
+        y: "-100vh",
+      },
+    };
+    menuItemVariants = {
+      open: {
+        y: 0,
+        opacity: 1,
+      },
+      closed: {
+        y: "-100vh",
+        opacity: 0,
+      },
+    };
+  }
+
+  const avtiveStyles = {
+    color: "var(--menu-active-text-color)",
+    backgroundColor: "var(--menu-active-background-color)",
+    borderRadius: "15px",
   };
-  const menuItemVariants = {
-    open: {
-      y: 0,
-      opacity: 1,
-    },
-    closed: {
-      y: "-100vh",
-      opacity: 0,
-    },
-  };
+
   return (
     <motion.div
       animate={props.isOpen ? "open" : "closed"}
@@ -31,25 +46,40 @@ export default function Menu(props) {
     >
       <motion.div
         variants={menuItemVariants}
-        className="menu-container__home center"
+        className="menu-container__home page-link"
       >
-        <Link to="/" onClick={toggleMenu}>
+        <Link
+          className="link-text"
+          to="/"
+          activeStyle={avtiveStyles}
+          onClick={toggleMenu}
+        >
           Home
         </Link>
       </motion.div>
       <motion.div
         variants={menuItemVariants}
-        className="menu-container__about center"
+        className="menu-container__about center page-link"
       >
-        <Link to="/about" onClick={toggleMenu}>
+        <Link
+          className="link-text"
+          to="/about"
+          activeStyle={avtiveStyles}
+          onClick={toggleMenu}
+        >
           About
         </Link>
       </motion.div>
       <motion.div
         variants={menuItemVariants}
-        className="menu-container__contact center"
+        className="menu-container__contact center page-link"
       >
-        <Link to="/contact" onClick={toggleMenu}>
+        <Link
+          className="link-text"
+          to="/contact"
+          activeStyle={avtiveStyles}
+          onClick={toggleMenu}
+        >
           Contact
         </Link>
       </motion.div>
