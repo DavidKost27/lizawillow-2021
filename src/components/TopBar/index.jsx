320e6dc2b0874ff4b0e1d301b61ca36bf2c163e0
David Kostuchenko <davidkost1999@gmail.com>
2021-04-22 12:57:34 +0300

    [Improvement] Menu and TopBar
    - Added overflow hidden when opening the menu so you wont have the option to scroll when it's not needed.
    - Made the topbar bigger by 20px on bigger devices (Experimental might change).

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -6,13 +6,21 @@ import Menu from "../Menu";
 export default function TopBar() {
   const [isOpen, setOpen] = useState(false);
   const toggleMenu = () => {
+    // get the body element tag
+    let body = document.getElementsByTagName("body")[0];
+
+    // apply the styles based on menu state
+    if (!isOpen) body.style.overflow = "hidden";
+    else body.style.overflow = "auto";
+
     setOpen(!isOpen);
   };
+
   return (
     <div className="topBar">
       <div className="topbar-container">
         <div className="topbar-container__logo">Liza Willow</div>
-        <span className="topbar-container__menu-btn">
+        <span className="topbar-container__menu-btn" onClick={toggleMenu}>
           <Hamburger size={29} rounded toggled={isOpen} toggle={setOpen} />
         </span>
         <Menu isOpen={isOpen} setOpen={setOpen} toggleMenu={toggleMenu} />
