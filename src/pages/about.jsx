4b04ee1f7bec6953eb6d53e15b6ac98bddd3642a
David Kostuchenko <davidkost1999@gmail.com>
2021-04-23 20:09:17 +0300

    [Update] Added initial about page layout

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import Layout from "../components/Layout/layout";
 import "../styles/about.scss";
+import Layout from "../components/Layout/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function About() {
   return (
     <Layout>
-      <div>About page</div>
+      <div className="about-container">
+        <div className="about-container__image">
+          <StaticImage
+            src="../assets/images/raster/dinoboy.jpeg"
+            alt="A dinosaur"
+            placeholder="blurred"
+            layout="fixed"
+            width={200}
+            height={200}
+            style={{ borderRadius: "15px" }}
+          />
+          <h1>About Me:</h1>
+        </div>
+        <div className="about-container__description">
+          <p>My name is Liza , I'm from Belarus</p>
+        </div>
+      </div>
     </Layout>
   );
 }
