6f19036d4092f183174a1bd58e91f4f13a34a4af
David Kostuchenko <davidkost1999@gmail.com>
2021-04-24 12:06:40 +0300

    [Update] Footer + fontAwsomeIcons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,54 @@
 import React from "react";
 import "./styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faDribbble,
+  faLinkedinIn,
+  faPatreon,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-  return <div className="footer">Foooter Boyyyyy</div>;
+  return (
+    <div className="footer">
+      {/* Social Media Icons */}
+      <div className="social-container">
+        <a href="https://www.instagram.com/lizawillow/">
+          <div className="social-container__icon-wrapper">
+            <FontAwesomeIcon icon={faInstagram} />
+          </div>
+        </a>
+
+        <div className="social-container__icon-wrapper">
+          <FontAwesomeIcon icon={faDribbble} />
+        </div>
+        <div className="social-container__icon-wrapper">
+          <FontAwesomeIcon
+            icon={faLinkedinIn}
+            style={{ padding: "0 0 0 2px" }}
+          />
+        </div>
+
+        <a href="https://www.instagram.com/lizawillow/">
+          <div className="social-container__icon-wrapper">
+            <FontAwesomeIcon
+              icon={faPatreon}
+              style={{ padding: "0 0 0 5px" }}
+            />
+          </div>
+        </a>
+      </div>
+      {/*  */}
+
+      {/* CopyRights */}
+      <div className="copyrights">
+        All rights reserved © Liza Willow , 2020 - 2021.
+      </div>
+      {/*  */}
+
+      {/* Built by */}
+      <div className="built-by">Website built by David Kostuchenko.</div>
+      {/*  */}
+    </div>
+  );
 }
